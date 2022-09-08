ffde0d42d7ec8f4ee1bf91d48b7218be3e9d8cd4
Roger Costa <roger.desenv@gmail.com>
2022-09-08 20:45:49 -0300

    customizando paleta de cores do app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+
 import App from './App'
+import './index.css'
+import { grey, amber } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+    secondary: {
+      main: amber[500],
+    },
+  },
+})
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App /> 
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
